6f1599cd42c4d48e354e910973c7a9ae575c4cfd
kaiquye <kaiquemendesilva82@gmail.com>
2025-03-20 20:16:10 -0300

    feat: adding swagger

--- src/main/java/br/com/kaique/Application.java
+++ src/main/java/br/com/kaique/Application.java
@@ -1,7 +1,17 @@
 package br.com.kaique;
 
 import io.micronaut.runtime.Micronaut;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.servers.Server;
 
+
+@OpenAPIDefinition(
+    info = @Info(
+        title = "creditcard-guides",
+        version = "1.0"
+    ), servers = @Server(url = "https://guides.micronaut.io")
+)
 public class Application {
 
   public static void main(String[] args) {
